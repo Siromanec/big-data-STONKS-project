892a8715a015fa9dac2f13fed93aee3a1763879a
Siromanec <serhii.ivanov@ucu.edu.ua>
2024-12-05 21:33:12 +0200

    docker fixes

--- producer/producer.py
+++ producer/producer.py
@@ -1,18 +1,21 @@
+import asyncio
+import json
+import os
+
+import pandas as pd
 import yfinance as yf
 from kafka import KafkaProducer
-import json
-import time
-import os
-import pandas as pd
 
 KAFKA_BOOTSTRAP_SERVERS = os.getenv('KAFKA_BOOTSTRAP_SERVERS', 'kafka:9092')
 TOPIC_NAME = 'stock_data_topic'
+
 
 # Custom JSON encoder to handle Pandas Timestamp
 def json_serializer(obj):
     if isinstance(obj, pd.Timestamp):
         return obj.isoformat()
     raise TypeError(f"Type {type(obj)} not serializable")
+
 
 # Initialize Kafka producer
 producer = KafkaProducer(
@@ -23,28 +26,36 @@ producer = KafkaProducer(
 # List of stock symbols to track
 stock_symbols = ['AAPL', 'GOOGL', 'MSFT']
 
+
 def fetch_and_send_stock_data():
+    for symbol in stock_symbols:
+        # Fetch stock data
+        stock = yf.Ticker(symbol)
+        data = stock.history(period='1m')
+        if not data.empty:
+            # Convert DataFrame to dict with proper serialization
+            latest_data = data.tail(1).reset_index().to_dict('records')[0]
+
+            # Convert Timestamp to string
+            if 'Date' in latest_data:
+                latest_data['Date'] = latest_data['Date'].isoformat()
+
+            message = {'symbol': symbol, 'data': latest_data}
+
+            # Send data to Kafka
+            producer.send(TOPIC_NAME, message)
+            print(f"Sent data for {symbol}: {message}")
+
+
+async def run_fetch_and_send_stock_data():
     while True:
-        for symbol in stock_symbols:
-            # Fetch stock data
-            stock = yf.Ticker(symbol)
-            data = stock.history(period='1m')
-            if not data.empty:
-                # Convert DataFrame to dict with proper serialization
-                latest_data = data.tail(1).reset_index().to_dict('records')[0]
-                
-                # Convert Timestamp to string
-                if 'Date' in latest_data:
-                    latest_data['Date'] = latest_data['Date'].isoformat()
-                
-                message = {'symbol': symbol, 'data': latest_data}
-                
-                # Send data to Kafka
-                producer.send(TOPIC_NAME, message)
-                print(f"Sent data for {symbol}: {message}")
-        
-        # Fetch data every minute
-        time.sleep(60)
+        try:
+            fetch_and_send_stock_data()
+            # Fetch data every minute
+            await asyncio.sleep(60)
+        except KeyboardInterrupt:
+            break
+
 
 if __name__ == '__main__':
-    fetch_and_send_stock_data()
+    asyncio.run(run_fetch_and_send_stock_data())
